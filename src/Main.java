4614ffe22494579f4f78b12a55690657c95af237
ICEtwd <kolomgames@gmail.com>
2022-08-28 18:07:24 +0500

    done

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,24 @@ import java.time.LocalDate;
 public class Main {
 
     //Task 1
-    public static int selectYear (){
-        int year = 2021;
+    private static int selectYear ( int year){
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && year %100 != 0) {
+            System.out.println(year + " - Год високосный");
+        }else {
+            System.out.println(year + " - Год не високосный");
+        }
+
         return year;
     }
 
     //Task 2
-    public static String getClientOS (String name) {
+    private static String getClientOS (String name) {
         if (name.equals("IOS")) {
-            return name;
         }
         return name;
     }
 
-    public static int getYearOS (int phoneYear) {
+    private static int getYearOS (int phoneYear) {
         int currentYear = LocalDate.now().getYear();
         if (phoneYear < currentYear) {
             System.out.print("Установите облегченную версию приложения для ");
@@ -29,7 +33,7 @@ public class Main {
 
     //Task 3
 
-    public static int deliver (int distance){
+    private static int getDeliver (int distance){
         int startInterval = 20;
         int interval = 40;
         int day = 1;
@@ -44,17 +48,14 @@ public class Main {
     public static void main(String[] args) {
 
         //Task 1
-            int total = selectYear();
-        if (total % 4 == 0 && total % 100 != 0 || total % 400 == 0 && total %100 != 0) {
-            System.out.println(total + " - Год високосный");
-        }else {
-            System.out.println(total + " - Год не високосный");
-        }
+
+        int year = 2024;
+        int total = selectYear(year);
 
         //Task 2
 
         String osName = "IOS";
-        int phYear = 2015;
+        int phYear = 2023;
         String clientOS = getClientOS(osName);
         int phoneYear = getYearOS(phYear);
         System.out.println(clientOS);
@@ -63,7 +64,7 @@ public class Main {
         //Task 3
 
         int lenght = 95;
-        int deliveryDays = deliver(lenght);
+        int deliveryDays = getDeliver(lenght);
         System.out.println("Доставка займет " + deliveryDays + " дня");
 
     }
